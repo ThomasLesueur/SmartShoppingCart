2df0d45d687c4c08584fbacb63326319f47441c7
Thomas.L <thomas.lesueur@epitech.eu>
2022-01-27 21:04:05 +0100

    refacto(product): refact views as function-based views

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from cart import views
 
 urlpatterns = [
     path('products/', views.product_list),
+    path('products/<int:pk>/', views.product_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- cart/views.py
+++ cart/views.py
@@ -1,23 +1,48 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from cart.models import Product
 from cart.serializers import ProductSerializer
 
-@csrf_exempt
-def product_list(request):
+@api_view(['GET', 'POST'])
+def product_list(request, format=None):
     """
     List all product, or create a new product.
     """
     if request.method == 'GET':
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ProductSerializer(data=data)
+        serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk, format=None):
+    """
+    Retrieve, update or delete a product.
+    """
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
