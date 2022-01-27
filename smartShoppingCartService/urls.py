bba3c10c7f61764a664baa68dd2874a987cd6e69
Thomas.L <thomas.lesueur@epitech.eu>
2022-01-27 18:57:18 +0100

    refacto(app): remove smartShoppingCart app
    refacto(app): remove smartShoppingChart app

--- smartShoppingCartService/urls.py
+++ smartShoppingCartService/urls.py
@@ -1,10 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
-from smartShoppingCartService.smartShoppingCart import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
