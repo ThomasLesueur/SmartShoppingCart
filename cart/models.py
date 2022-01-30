f272f7cb318d330afb74eed205a8c45e66004afb
Thomas.L <thomas.lesueur@epitech.eu>
2022-01-30 17:42:45 +0100

    add image in Product model

--- cart/models.py
+++ cart/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField(default=0)
-    
+    image = models.ImageField(blank=True)    
     class Meta:
         ordering = ['name']
 
